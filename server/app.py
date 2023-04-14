24a650c03a71c38287b366d5a8c88c0c6b49ba51
Denis <sotnikovdenis04@gmail.com>
2023-04-14 16:39:54 +0500

    create server folder and add app.py

---
+++ server/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from server.domain import db_session
+from server.domain.user import User
+from server.repository.user_repository import UserRepository
+
+app = Flask(__name__)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    params = request.json
+    user = User(**params)
+    user_repository = UserRepository()
+    user_repository.add(user)
+    token = user.get_token()
+    return {'access_token': token}
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    params = request.json
+    user = User.authenticate(**params)
+    token = user.get_token()
+    return {'access_token': token}
+
+
+if __name__ == '__main__':
+    db_session.global_init()
