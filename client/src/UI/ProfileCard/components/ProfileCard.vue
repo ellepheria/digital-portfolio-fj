43cd64b64711314053dc5112b601d8bc1aff784d
Maxim Mineev <mms1401@ya.ru>
2023-05-26 01:58:28 +0500

    add profile card component

--- client/src/UI/ProfileCard/components/ProfileCard.vue
+++ client/src/UI/ProfileCard/components/ProfileCard.vue
@@ -1,13 +1,120 @@
 <template>
-
+  <div class="card-container">
+    <div class="card-preview">
+      <div class="cover-container">
+        <img
+            v-if="coverUploaded"
+            :src="cover_path"
+            class="cover"
+        >
+      </div>
+      <div class="profile-picture-container">
+        <img
+            v-if="profilePictureUploaded"
+            :src="profile_picture_path"
+            class="profile-picture">
+        <img
+            v-if="!profilePictureUploaded"
+            src="@/assets/defaultProfilePicture.png"
+            class="profile-picture">
+      </div>
+      <div class="text-fields">
+        <p class="name-data text-field">{{ username }}</p>
+        <p class="type_of_activity-data text-field">{{ type_of_activity }}</p>
+      </div>
+      <p class="about-data text-field">{{ about }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ProfileCard"
+  name: "ProfileCard",
+  props: {
+    username: '',
+    about: '',
+    type_of_activity: '',
+    profile_picture_path: '',
+    cover_path: '',
+  },
+  computed: {
+    profilePictureUploaded(){
+      return !!this.$props.profile_picture_path;
+    },
+    coverUploaded() {
+      return !!this.$props.cover_path
+    }
+  }
 }
 </script>
 
 <style scoped>
+.card-container {
+  display: flex;
+  flex-direction: column;
+  height: 770px;
+  width: 502px;
+}
 
+.card-preview {
+  display: flex;
+  flex-direction: column;
+  background: #C3C3C3;
+  height: 548px;
+  border-radius: 40px;
+  position: relative;
+}
+
+.cover-container {
+  background: #eee;
+  height: 200px;
+  width: 100%;
+  border-radius: 40px 40px 0 0;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.cover {
+  height: 100%;
+  width: 100%;
+  border-radius: 40px 40px 0 0;
+  object-fit: cover;
+}
+
+.profile-picture-container {
+  border-radius: 50%;
+  height: 130px;
+  width: 130px;
+  justify-content: center;
+  position: absolute;
+  left: 185.5px;
+  top: 142.5px;
+
+}
+
+.profile-picture {
+  height: 100%;
+  width: 100%;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.text-fields {
+  margin-top: 82px;
+  display: flex;
+  flex-direction: column;
+}
+
+.text-field {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 160%;
+  text-align: center;
+  color: #FFFFFF;
+}
 </style>
