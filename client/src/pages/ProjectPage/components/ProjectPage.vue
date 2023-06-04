d364acf04b981e0907ffb4f850a0e180886e22b8
Maxim Mineev <mms1401@ya.ru>
2023-06-04 19:38:56 +0500

    add slider component

--- client/src/pages/ProjectPage/components/ProjectPage.vue
+++ client/src/pages/ProjectPage/components/ProjectPage.vue
@@ -1,6 +1,7 @@
 <template>
   <Header></Header>
   <div class="main-container">
+    <slider></slider>
     {{title}}
     {{short_description}}
     {{description}}
@@ -15,10 +16,11 @@
 import Header from "@/UI/Header/components/Header.vue";
 import Footer from "@/UI/Footer/components/Footer.vue";
 import $http, {baseURI} from "@/api";
+import Slider from "@/modules/Slider/components/Slider.vue";
 
 export default {
   name: "ProjectPage",
-  components: {Footer, Header},
+  components: {Slider, Footer, Header},
   data() {
     return {
       title: '',
