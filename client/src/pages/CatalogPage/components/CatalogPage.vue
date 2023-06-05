15de1574d7df83ae4d1e10a5c1d6b01b364a9af0
Maxim Mineev <mms1401@ya.ru>
2023-06-05 18:22:44 +0500

    add endless feed in catalog page

--- client/src/pages/CatalogPage/components/CatalogPage.vue
+++ client/src/pages/CatalogPage/components/CatalogPage.vue
@@ -4,8 +4,8 @@
     <profiles-list
         :profiles-list="userCards"
     >
-
     </profiles-list>
+    <div ref="observer" class="observer"></div>
   </div>
   <Footer></Footer>
 </template>
@@ -26,13 +26,20 @@ export default {
       userCards: [],
     };
   },
-  async created(){
-    const cards = (await this.getUserCards()).data;
-    for (let i = 0; i < cards.length; i++) {
-      cards[i]['images'].cover_path = baseURI + cards[i]['images'].cover_path;
-      cards[i]['images'].profile_picture_path = baseURI + cards[i]['images'].profile_picture_path;
-    }
-    this.userCards = [...this.userCards, ...cards];
+  mounted() {
+    const options = {
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting) {
+        this.getUserCards();
+      }
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
   },
   methods: {
     async getUserCards() {
@@ -42,7 +49,12 @@ export default {
         count: this.count,
       };
       this.page++;
-      return await $http.get(url, {params: params});
+      const cards = (await $http.get(url, {params: params})).data;
+      for (let i = 0; i < cards.length; i++) {
+        cards[i]['images'].cover_path = baseURI + cards[i]['images'].cover_path;
+        cards[i]['images'].profile_picture_path = baseURI + cards[i]['images'].profile_picture_path;
+      }
+      this.userCards = [...this.userCards, ...cards];
     }
   }
 }
@@ -51,5 +63,9 @@ export default {
 <style scoped>
 .user-cards-container{
   margin: 40px;
+}
+.observer {
+  height: 30px;
+  background: none;
 }
 </style>
